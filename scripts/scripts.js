92859299f260a33e743f17d2a0e273ccd95ae9da
Pedro Luis Schultz Sarmento <106385985+PLSchultz@users.noreply.github.com>
2023-06-01 22:01:36 -0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function index(){
+    window.location.href = "index.html";
+}
+
+function menu(){
+    window.location.href = "menu.html";
+}
+
+function infos(){
+    window.location.href = "infos.html";
+}
